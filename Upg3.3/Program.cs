f47e3639bc84cb32877f1ef8163f4c901f219056
nehy06001 <nehy06001@E-PW05CGQV>
2024-11-14 15:37:18 +0100

    Add project files.

---
+++ Upg3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace upg3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timpris = 80;
+            int maxdagkostnad = 950;
+
+            Console.WriteLine("Hur många timmar vill du hyra bilen? ");
+            int timmar = int.Parse(Console.ReadLine());
+
+            int kostnad = timmar * timpris;
+
+            if (kostnad > maxdagkostnad)
+            {
+                kostnad = maxdagkostnad;
+            }
+
+            Console.WriteLine("Det kommer att kosta: " + kostnad + " kr");
+        }
+    }
+}
